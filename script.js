4c78215a74e068cd841d3da57be445ae509a9f95
J-Groesbeck <jacob.groesbeck@icloud.com>
2023-12-07 13:45:58 -0500

    Reordered Js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let adjective1 = prompt('Enter an adjective:');
-let noun1 = prompt('Enter an noun:');
 let place1 = prompt('Enter an place:');
 let verb1 = prompt('Enter an verb:');
-let verb2 = prompt('Enter an verb:');
+let noun1 = prompt('Enter an noun:');
 let adverb1 = prompt('Enter an adverb:');
+let verb2 = prompt('Enter an verb:');
 
 let story = `Once upon a time, in a ${adjective1} ${place1}, a ${verb1} ${noun1} ${adverb1} ${verb2}.`;
 
