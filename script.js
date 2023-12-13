a043c129a70797898cf06b7caabaaf50da9837a7
J-Groesbeck <jacob.groesbeck@icloud.com>
2023-12-13 13:11:52 -0500

    Added Start Button

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let adjective1 = prompt('Enter an adjective:');
-let place1 = prompt('Enter an place:');
-let verb1 = prompt('Enter an verb:');
-let noun1 = prompt('Enter an noun:');
-let adverb1 = prompt('Enter an adverb:');
-let verb2 = prompt('Enter an verb:');
+function startMadLibs() {
+    let adjective1 = prompt('Enter an adjective:');
+    let place1 = prompt('Enter an place:');
+    let verb1 = prompt('Enter an verb:');
+    let noun1 = prompt('Enter an noun:');
+    let adverb1 = prompt('Enter an adverb:');
+    let verb2 = prompt('Enter an verb:');
 
-let story = `Once upon a time, in a ${adjective1} ${place1}, a ${verb1} ${noun1} ${adverb1} ${verb2}.`;
+    let story = `Once upon a time, in a ${adjective1} ${place1}, a ${verb1} ${noun1} ${adverb1} ${verb2}.`;
 
-let madLibOutputDiv = document.getElementById('madLibOutput');
+    let madLibOutputDiv = document.getElementById('madLibOutput');
 
-madLibOutputDiv.innerHTML = `<p>${story}<p>`
+    madLibOutputDiv.innerHTML = `<p>${story}<p>`
+}
