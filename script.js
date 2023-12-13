23ed8742e40eb57791e57a6546604f751c1d21f1
J-Groesbeck <jacob.groesbeck@icloud.com>
2023-12-13 13:25:32 -0500

    bolded words

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function startMadLibs() {
     let adverb1 = prompt('Enter an adverb:');
     let verb2 = prompt('Enter an verb:');
 
-    let story = `Once upon a time, in a ${adjective1} ${place1}, a ${verb1} ${noun1} ${adverb1} ${verb2}.`;
+    let story = `Once upon a time, in a <span class="bold">${adjective1}</span> <span class="bold">${place1}</span>, a <span class="bold">${verb1}</span> <span class="bold">${noun1}</span> <span class="bold">${adverb1}</span> <span class="bold">${verb2}</span>.`;
 
     let madLibOutputDiv = document.getElementById('madLibOutput');
 
